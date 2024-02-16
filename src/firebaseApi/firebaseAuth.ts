d18a95315ed4ebc6ea53d0dbb3dea175463d1012
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-02-16 18:05:26 +0700

    starting

---
+++ src/firebaseApi/firebaseAuth.ts
@@ -0,0 +1,32 @@
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword
+} from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+
+import toast from 'react-hot-toast';
+
+import { auth, fireStore } from '../firebase-config';
+
+export function fbRegister(email: string, password: string) {
+	return createUserWithEmailAndPassword(auth, email, password)
+		.then(credentials => credentials.user)
+		.then(user => {
+			setDoc(doc(fireStore, 'users', email), {
+				favourite: [],
+				history: []
+			});
+			return user;
+		})
+		.catch(error => {
+			toast.error(error.message);
+		});
+}
+
+export function fbLogin(email: string, password: string) {
+	return signInWithEmailAndPassword(auth, email, password)
+		.then(credentials => credentials.user)
+		.catch(error => {
+			toast.error(error.message);
+		});
+}
