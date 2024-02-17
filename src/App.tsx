f30d5ad8dd897e15fbcc27d88528ac4075f14673
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-02-17 18:00:29 +0700

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-import Preloader from "./ui/elements/Preloader/Preloader";
 import { Header } from "./ui/components/Header/Header";
-import { SignIn } from "./pages/SignIn/SignIn";
 import { SignUp } from "./pages/SignUp/SignUp";
 
 function App() {
