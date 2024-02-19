f26fb8c8451c86fb0d9c63b9247ceab4a44848e5
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-02-19 23:36:12 +0700

    add firebase config

---
+++ src/firebase-config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbPYPgiIxG7E_x0a_izCBT4lR-uycAMuc",
+  authDomain: "aston-react-93b87.firebaseapp.com",
+  databaseURL: "https://aston-react-93b87-default-rtdb.firebaseio.com",
+  projectId: "aston-react-93b87",
+  storageBucket: "aston-react-93b87.appspot.com",
+  messagingSenderId: "451658867684",
+  appId: "1:451658867684:web:e2231b80ba0efab2549df1",
+  measurementId: "G-KR1FEVLRCV",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const fireStore = getFirestore(app);
