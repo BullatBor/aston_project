b6585005419a7ea9440961b81347bf1402134fde
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-02-19 22:10:14 +0700

    fix prettier error

--- src/store/auth/authSlice.ts
+++ src/store/auth/authSlice.ts
@@ -1,43 +1,39 @@
-
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { User } from 'firebase/auth';
-import { RootState } from '../store';
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "firebase/auth";
+import { RootState } from "../store";
 
 interface initialStateType {
-    user: User | null
-    isLoading: boolean
-    isAuth: boolean
+  user: User | null;
+  isLoading: boolean;
+  isAuth: boolean;
 }
 
 let initialState: initialStateType = {
-    user: null,
-    isLoading: false,
-    isAuth: false
-}
-
+  user: null,
+  isLoading: false,
+  isAuth: false,
+};
 
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        setUser(state, action: PayloadAction<User>) {
-            state.user = action.payload
-            state.isAuth = true
-        },
-        deleteUser(state) {
-            state.user = null
-            state.isAuth = false
-        },
-        setIsLoading: (state, action: PayloadAction<boolean>) => {
-			state.isLoading = action.payload;
-		}
-    }
-})
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<User>) {
+      state.user = action.payload;
+      state.isAuth = true;
+    },
+    deleteUser(state) {
+      state.user = null;
+      state.isAuth = false;
+    },
+    setIsLoading: (state, action: PayloadAction<boolean>) => {
+      state.isLoading = action.payload;
+    },
+  },
+});
 
 export const isLoading = (state: RootState) => state.userReducer.isLoading;
 export const user = (state: RootState) => state.userReducer.user;
-
 
 export const { setUser, deleteUser, setIsLoading } = userSlice.actions;
 export default userSlice.reducer;
