f739c8cf7b0ea97499c85e484f5c04ca493a6475
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-02-24 15:13:09 +0700

    fix prettier error

--- src/hooks/useFavourites.ts
+++ src/hooks/useFavourites.ts
@@ -14,7 +14,6 @@ export const useFavourites = () => {
 
   const [addInFavourite, addResult] = favouriteApi.useAddToFavouriteMutation();
 
-
   const [removeFavourite, removeResult] =
     favouriteApi.useRemoveFromFavouriteMutation();
 
