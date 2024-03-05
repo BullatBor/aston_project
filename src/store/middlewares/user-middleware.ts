e05a5751bd420565c7f31b6e5d5ed2eb849b5e5f
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-03-05 15:31:28 +0700

    add middleware Listener

---
+++ src/store/middlewares/user-middleware.ts
@@ -0,0 +1,20 @@
+import { createListenerMiddleware } from '@reduxjs/toolkit';
+import toast from 'react-hot-toast';
+
+import { setUser, logout } from '../auth/authSlice';
+
+export const listenerMiddleware = createListenerMiddleware();
+
+listenerMiddleware.startListening({
+  actionCreator: setUser,
+  effect: (action) => {
+    toast.success(`Вы авторизовались: ${action.payload.email}`);
+  },
+});
+
+listenerMiddleware.startListening({
+  actionCreator: logout,
+  effect: () => {
+    toast.success('Вы вышли из системы');
+  },
+});
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
-import { combineReducers } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./auth/authSlice";
-import { movieApi } from "./rtkQuery/movieApi";
-import { favouriteApi } from "./rtkQuery/favoritesApi";
-import { historyApi } from "./rtkQuery/historyApi";
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './auth/authSlice';
+import { movieApi } from './rtkQuery/movieApi';
+import { favouriteApi } from './rtkQuery/favoritesApi';
+import { historyApi } from './rtkQuery/historyApi';
+import { listenerMiddleware } from './middlewares/user-middleware';
 
 const rootReducer = combineReducers({
   [movieApi.reducerPath]: movieApi.reducer,
@@ -16,12 +17,11 @@ const setupStore = () =>
   configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: false,
-      }).concat([
+      getDefaultMiddleware().concat([
         movieApi.middleware,
         favouriteApi.middleware,
         historyApi.middleware,
+        listenerMiddleware.middleware,
       ]),
   });
 
@@ -29,4 +29,4 @@ export const store = setupStore();
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
-export type AppDispatch = AppStore["dispatch"];
+export type AppDispatch = AppStore['dispatch'];
