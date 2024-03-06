eca294ae2711c8b5796f8ec30e7cdf78d0fbd618
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-03-06 17:18:03 +0700

    fix build error

--- src/hooks/useFireStore.ts
+++ src/hooks/useFireStore.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { isAuth, user } from '../store/auth/authSlice';
 import { favouriteApi } from '../store/rtkQuery/favoritesApi';
