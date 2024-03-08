0dde30e07e4bdda5b2118542a4a65cc12db368c7
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-03-08 17:40:45 +0700

    fix prettier format

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { movieApi } from '../../store/rtkQuery/movieApi';
-import MovieCard from '../../ui/components/MovieCard/MovieCard';
-import Preloader from '../../ui/elements/Preloader/Preloader';
-import s from './searchPage.module.css';
-import { EmptyTitle } from '../../ui/components/EmptyTitle/EmptyTitle';
-import { useHistory } from '../../hooks/useHistory';
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { movieApi } from "../../store/rtkQuery/movieApi";
+import MovieCard from "../../ui/components/MovieCard/MovieCard";
+import Preloader from "../../ui/elements/Preloader/Preloader";
+import s from "./searchPage.module.css";
+import { EmptyTitle } from "../../ui/components/EmptyTitle/EmptyTitle";
+import { useHistory } from "../../hooks/useHistory";
 
 const SearchPage = () => {
   const location = useLocation();
   const { SearchTextChanged } = useHistory();
   const queryParams = new URLSearchParams(location.search);
 
-  const query = queryParams.get('searchText');
+  const query = queryParams.get("searchText");
 
   useEffect(() => {
     if (query) {
@@ -32,7 +32,7 @@ const SearchPage = () => {
         searchResult.length > 0 ? (
           searchResult.map((item) => <MovieCard key={item.id} {...item} />)
         ) : (
-          <EmptyTitle title='Ничего не нашлось' />
+          <EmptyTitle title="Ничего не нашлось" />
         )
       ) : (
         <Preloader width={45} />
