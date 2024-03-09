8634daee0fa86561e70e2ea196777ab56027fd58
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-03-09 12:29:15 +0700

    first

--- src/ui/components/Suggest/Suggest.tsx
+++ src/ui/components/Suggest/Suggest.tsx
@@ -1,11 +1,12 @@
-import React, { FC, LegacyRef } from "react";
-import { ICollection } from "../../../models/ICollection";
-import Preloader from "../../elements/Preloader/Preloader";
-import s from "./Suggest.module.css";
-import { SuggestItem } from "./SuggestItem/SuggestItem";
-import cn from "classnames";
-import { useTheme } from "../../../context/ThemeContext";
-import { EmptyTitle } from "../EmptyTitle/EmptyTitle";
+import { FC, LegacyRef } from 'react';
+import { ICollection } from '../../../models/ICollection';
+import Preloader from '../../elements/Preloader/Preloader';
+import s from './Suggest.module.css';
+import { SuggestItem } from './SuggestItem/SuggestItem';
+import cn from 'classnames';
+import { useTheme } from '../../../context/ThemeContext';
+import { EmptyTitle } from '../EmptyTitle/EmptyTitle';
+import { FixedSizeList as List } from 'react-window';
 
 interface SuggestProps {
   movies: ICollection[] | null | undefined;
@@ -18,16 +19,25 @@ export const Suggest: FC<SuggestProps> = ({ movies, suggestRef }) => {
   return (
     <div
       className={cn(s.wrapper, {
-        [s.dark]: theme === "dark",
-        [s.light]: theme === "light",
+        [s.dark]: theme === 'dark',
+        [s.light]: theme === 'light',
       })}
       ref={suggestRef}
     >
       {movies ? (
         movies.length > 0 ? (
-          movies.map((item) => <SuggestItem key={item.id} {...item} />)
+          <List
+            height={100}
+            itemCount={movies.length}
+            itemSize={35}
+            width={200}
+          >
+            {({ index, style }) => (
+              <SuggestItem key={movies[index].id} {...movies[index]} />
+            )}
+          </List>
         ) : (
-          <EmptyTitle title="Ничего не нашлось" classNames={s.emptyTitle} />
+          <EmptyTitle title='Ничего не нашлось' classNames={s.emptyTitle} />
         )
       ) : (
         <div className={s.preloader}>
